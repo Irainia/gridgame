f58b0497b9858dc5d49381e7205ddc74ee82e4af
Anwar Hidayat <anwarhidayat14@gmail.com>
2019-01-16 22:36:06 +0700

    add game board as the place to put shape

---
+++ board/gridboard.go
@@ -0,0 +1,179 @@
+package board
+
+import (
+	"errors"
+
+	"github.com/irainia/gridgame/position"
+	"github.com/irainia/gridgame/shape"
+)
+
+// EDoubleDirection will direct doubling the board
+type EDoubleDirection int
+
+const (
+	// ToRight will direct double to right
+	ToRight EDoubleDirection = iota
+	// ToButtom will direct double to buttom
+	ToButtom
+)
+
+// GameBoard will manage board
+type GameBoard struct {
+	row, column  *int
+	gridBoard    [][]bool
+	listShape    []shape.IShape
+	listPosition []position.Position
+}
+
+// NewGameBoard will create Game
+func NewGameBoard(row, column int) (*GameBoard, error) {
+	if row <= 0 {
+		return nil, errors.New("row should be more than zero")
+	}
+	if column <= 0 {
+		return nil, errors.New("column should be more than zero")
+	}
+
+	gridBoard := make([][]bool, row)
+	for i := 0; i < row; i++ {
+		gridBoard[i] = make([]bool, column)
+	}
+
+	output := GameBoard{}
+	output.row, output.column = &row, &column
+	output.gridBoard = gridBoard
+	return &output, nil
+}
+
+// AddShape will add shape to the board
+func (g *GameBoard) AddShape(sp shape.IShape, pos *position.Position) (bool, error) {
+	if g.row == nil || g.column == nil {
+		return false, errors.New("board is not initialized")
+	}
+	if sp == nil {
+		return false, errors.New("shape should not be a nil value")
+	}
+	if pos == nil {
+		return false, errors.New("position should not be a nil value")
+	}
+
+	posCoordinate, err := pos.GetPosition()
+	if err != nil {
+		return false, errors.New("position is not initialized")
+	}
+
+	if posCoordinate[0] >= *g.row || posCoordinate[1] >= *g.column {
+		return false, errors.New("position exceed board")
+	}
+
+	spRow, spColumn := sp.GetSize()
+	if spRow == -1 || spColumn == -1 {
+		return false, errors.New("shape is not initialized")
+	}
+
+	if posCoordinate[0]+spRow-1 >= *g.row ||
+		posCoordinate[1]+spColumn-1 >= *g.column {
+		return false, errors.New("position exceed board")
+	}
+
+	spOccupied := sp.GetOccupiedArea()
+	for i := 0; i < spRow; i++ {
+		for j := 0; j < spColumn; j++ {
+			if spOccupied[i][j] {
+				if g.gridBoard[i+posCoordinate[0]][j+posCoordinate[1]] {
+					return false, errors.New("overlap adding shape")
+				}
+			}
+		}
+	}
+
+	for i := 0; i < spRow; i++ {
+		for j := 0; j < spColumn; j++ {
+			if spOccupied[i][j] {
+				g.gridBoard[i+posCoordinate[0]][j+posCoordinate[1]] = spOccupied[i][j]
+			}
+		}
+	}
+
+	g.listShape = append(g.listShape, sp)
+	g.listPosition = append(g.listPosition, *pos)
+
+	return true, nil
+}
+
+// DoubleBoard will double the board based on direction
+func (g *GameBoard) DoubleBoard(direct EDoubleDirection) bool {
+	if g.row == nil || g.column == nil {
+		return false
+	}
+	var newBoard [][]bool
+	var newRow, newColumn int
+
+	switch direct {
+	case ToRight:
+		newRow = *g.row
+		newColumn = *g.column * 2
+	case ToButtom:
+		newRow = *g.row * 2
+		newColumn = *g.column
+	default:
+		return false
+	}
+
+	newBoard = make([][]bool, newRow)
+	for i := 0; i < newRow; i++ {
+		newBoard[i] = make([]bool, newColumn)
+	}
+
+	for i := 0; i < *g.row; i++ {
+		for j := 0; j < *g.column; j++ {
+			newBoard[i][j] = g.gridBoard[i][j]
+		}
+	}
+
+	g.gridBoard = newBoard
+	g.row = &newRow
+	g.column = &newColumn
+
+	return true
+}
+
+// CalculateOccupiedArea will return number of area occupied
+func (g *GameBoard) CalculateOccupiedArea() int {
+	if g.row == nil || g.column == nil {
+		return -1
+	}
+
+	output := 0
+	for i := 0; i < *g.row; i++ {
+		for j := 0; j < *g.column; j++ {
+			if g.gridBoard[i][j] {
+				output++
+			}
+		}
+	}
+
+	return output
+}
+
+// GetOccupiedArea will return occupied area on the board
+func (g *GameBoard) GetOccupiedArea() [][]bool {
+	if g.row == nil || g.column == nil {
+		return nil
+	}
+
+	output := make([][]bool, *g.row)
+	for i := 0; i < *g.row; i++ {
+		output[i] = make([]bool, *g.column)
+	}
+	copy(output, g.gridBoard)
+	return output
+}
+
+// GetSize will return board size (row, column)
+func (g *GameBoard) GetSize() (int, int) {
+	if g.row == nil || g.column == nil {
+		return -1, -1
+	}
+	return *g.row, *g.column
+}
---
+++ board/gridboard_test.go
@@ -0,0 +1,391 @@
+package board_test
+
+import (
+	"testing"
+
+	"github.com/irainia/gridgame/board"
+	"github.com/irainia/gridgame/position"
+	rectangle "github.com/irainia/gridgame/shape/rectangle"
+	triangle "github.com/irainia/gridgame/shape/triangle"
+)
+
+func TestNewGameBoardRowIsNegative(t *testing.T) {
+	_, err := board.NewGameBoard(-1, 1)
+	expected := "row should be more than zero"
+
+	if err == nil {
+		t.Errorf(expected)
+		return
+	}
+
+	if err.Error() != expected {
+		t.Errorf("expected: %s - actual: %s", expected, err.Error())
+	}
+}
+
+func TestNewGameBoardRowIsZero(t *testing.T) {
+	_, err := board.NewGameBoard(0, 1)
+	expected := "row should be more than zero"
+
+	if err == nil {
+		t.Errorf(expected)
+		return
+	}
+
+	if err.Error() != expected {
+		t.Errorf("expected: %s - actual: %s", expected, err.Error())
+	}
+}
+
+func TestNewNewGameBoardIsNegative(t *testing.T) {
+	_, err := board.NewGameBoard(1, -1)
+	expected := "column should be more than zero"
+
+	if err == nil {
+		t.Errorf(expected)
+		return
+	}
+
+	if err.Error() != expected {
+		t.Errorf("expected: %s - actual: %s", expected, err.Error())
+	}
+}
+
+func TestNewGameBoardColumnIsZero(t *testing.T) {
+	_, err := board.NewGameBoard(1, 0)
+	expected := "column should be more than zero"
+
+	if err == nil {
+		t.Errorf(expected)
+		return
+	}
+
+	if err.Error() != expected {
+		t.Errorf("expected: %s - actual: %s", expected, err.Error())
+	}
+}
+
+func TestAddShapeWithBoardNotInitialized(t *testing.T) {
+	brd := board.GameBoard{}
+	sp, _ := rectangle.NewRectangle(1, 1)
+	pos, _ := position.NewPosition(0, 0)
+	_, err := brd.AddShape(sp, pos)
+	expected := "board is not initialized"
+	if err == nil {
+		t.Errorf(expected)
+		return
+	}
+	if err.Error() != expected {
+		t.Errorf("expected: %s - actual: %s", expected, err.Error())
+	}
+}
+
+func TestAddShapeWithShapeIsNil(t *testing.T) {
+	brd, _ := board.NewGameBoard(10, 10)
+	pos, _ := position.NewPosition(0, 0)
+	_, err := brd.AddShape(nil, pos)
+	expected := "shape should not be a nil value"
+	if err == nil {
+		t.Errorf(expected)
+		return
+	}
+	if err.Error() != expected {
+		t.Errorf("expected: %s - actual: %s", expected, err.Error())
+	}
+}
+
+func TestAddShapeWithShapeNotInitialized(t *testing.T) {
+	brd, _ := board.NewGameBoard(10, 10)
+	pos, _ := position.NewPosition(0, 0)
+	_, err := brd.AddShape(rectangle.Rectangle{}, pos)
+	expected := "shape is not initialized"
+	if err == nil {
+		t.Errorf(expected)
+		return
+	}
+	if err.Error() != expected {
+		t.Errorf("expected: %s - actual: %s", expected, err.Error())
+	}
+}
+
+func TestAddShapeWithPositionIsNil(t *testing.T) {
+	brd, _ := board.NewGameBoard(10, 10)
+	sp, _ := rectangle.NewRectangle(2, 2)
+	_, err := brd.AddShape(sp, nil)
+	expected := "position should not be a nil value"
+	if err == nil {
+		t.Errorf(expected)
+		return
+	}
+	if err.Error() != expected {
+		t.Errorf("expected: %s - actual: %s", expected, err.Error())
+	}
+}
+
+func TestAddShapeWithPositionNotInitialized(t *testing.T) {
+	brd, _ := board.NewGameBoard(10, 10)
+	pos := position.Position{}
+	sp, _ := rectangle.NewRectangle(2, 2)
+	_, err := brd.AddShape(sp, &pos)
+	expected := "position is not initialized"
+	if err == nil {
+		t.Errorf(expected)
+		return
+	}
+	if err.Error() != expected {
+		t.Errorf("expected: %s - actual: %s", expected, err.Error())
+	}
+}
+
+func TestAddShapePositionExceedBoard(t *testing.T) {
+	sp, _ := rectangle.NewRectangle(3, 3)
+
+	brd, _ := board.NewGameBoard(10, 10)
+	ps, _ := position.NewPosition(10, 10)
+	status, err := brd.AddShape(sp, ps)
+
+	expectedStatus := false
+	expectedError := "position exceed board"
+
+	if err == nil {
+		t.Errorf("expected: (false, \"%s\") - actual: (%t,nil)",
+			expectedError, status)
+		return
+	}
+
+	if status != expectedStatus || err.Error() != expectedError {
+		t.Errorf("expected: (false, \"%s\") - actual: (%t,\"%s\")",
+			expectedError, status, err.Error())
+	}
+}
+
+func TestAddShapeShouldSuccess(t *testing.T) {
+	sp, _ := rectangle.NewRectangle(3, 3)
+
+	brd, _ := board.NewGameBoard(10, 10)
+	pos, _ := position.NewPosition(0, 0)
+	status, err := brd.AddShape(sp, pos)
+	expected := "expected: (true, nil)"
+
+	if !status || err != nil {
+		t.Errorf(expected)
+	}
+}
+
+func TestAddShapePositionWithShapeExceedBoard(t *testing.T) {
+	sp, _ := rectangle.NewRectangle(3, 3)
+
+	brd, _ := board.NewGameBoard(10, 10)
+	ps, _ := position.NewPosition(8, 8)
+	status, err := brd.AddShape(sp, ps)
+
+	expectedStatus := false
+	expectedError := "position exceed board"
+
+	if err == nil {
+		t.Errorf("expected: (false, \"%s\") - actual: (%t,nil)",
+			expectedError, status)
+		return
+	}
+
+	if status != expectedStatus || err.Error() != expectedError {
+		t.Errorf("expected: (false, \"%s\") - actual: (%t,\"%s\")",
+			expectedError, status, err.Error())
+	}
+}
+
+func TestAddShapeNotOverlap(t *testing.T) {
+	rect, _ := rectangle.NewRectangle(4, 1)
+	ltriangle, _ := triangle.NewLeftButtomTriangle(3, 3)
+	brd, _ := board.NewGameBoard(4, 4)
+	pos := position.Position{}
+	brd.AddShape(rect, &pos)
+
+	ps, _ := position.NewPosition(0, 1)
+	status, _ := brd.AddShape(ltriangle, ps)
+	if !status {
+		t.Errorf("adding shape should success")
+	}
+}
+
+func TestAddShapeOverlap(t *testing.T) {
+	ltriangle, _ := triangle.NewLeftButtomTriangle(3, 3)
+	rect, _ := rectangle.NewRectangle(4, 1)
+
+	brd, _ := board.NewGameBoard(4, 4)
+	pos, _ := position.NewPosition(0, 0)
+	brd.AddShape(ltriangle, pos)
+	pos, _ = position.NewPosition(0, 0)
+	status, err := brd.AddShape(rect, pos)
+
+	expectedStatus := false
+	expectedError := "overlap adding shape"
+	if err == nil {
+		t.Errorf("expected: (false, \"%s\") - actual: (%t,nil)",
+			expectedError, status)
+		return
+	}
+
+	if status != expectedStatus || err.Error() != expectedError {
+		t.Errorf("expected: (false, \"%s\") - actual: (%t,\"%s\")",
+			expectedError, status, err.Error())
+	}
+}
+
+func TestGetOccupiedAreaFail(t *testing.T) {
+	brd := board.GameBoard{}
+	occ := brd.GetOccupiedArea()
+	if occ != nil {
+		t.Errorf("expected: nil - actuL: %v", occ)
+	}
+}
+func TestGetOccupiedAreaEmpty(t *testing.T) {
+	brd, _ := board.NewGameBoard(3, 3)
+	actual := brd.GetOccupiedArea()
+	expected := [][]bool{
+		{false, false, false},
+		{false, false, false},
+		{false, false, false},
+	}
+
+	row, column := brd.GetSize()
+	isSuccess := true
+	for i := 0; i < row; i++ {
+		if isSuccess {
+			for j := 0; j < column; j++ {
+				isSuccess = actual[i][j] == expected[i][j]
+				if !isSuccess {
+					break
+				}
+			}
+		}
+	}
+
+	if !isSuccess {
+		t.Errorf("board should be empty")
+	}
+}
+
+func TestGetOccupiedAreaNotEmpty(t *testing.T) {
+	sp, _ := triangle.NewLeftButtomTriangle(3, 3)
+	brd, _ := board.NewGameBoard(4, 4)
+	pos, _ := position.NewPosition(0, 0)
+	brd.AddShape(sp, pos)
+
+	actual := brd.GetOccupiedArea()
+	expected := [][]bool{
+		{true, false, false, false},
+		{true, true, false, false},
+		{true, true, true, false},
+		{false, false, false, false},
+	}
+
+	row, column := brd.GetSize()
+	isSuccess := true
+	for i := 0; i < row; i++ {
+		if isSuccess {
+			for j := 0; j < column; j++ {
+				isSuccess = actual[i][j] == expected[i][j]
+				if !isSuccess {
+					break
+				}
+			}
+		}
+	}
+
+	if !isSuccess {
+		t.Errorf("board should be the same")
+	}
+}
+
+func TestDoubleBoardUnderMinimimum(t *testing.T) {
+	board, _ := board.NewGameBoard(2, 2)
+	status := board.DoubleBoard(-1)
+	if status {
+		t.Errorf("double board should fail (false)")
+	}
+}
+
+func TestDoubleBoardOverMaximum(t *testing.T) {
+	brd, _ := board.NewGameBoard(2, 2)
+	status := brd.DoubleBoard(2)
+	if status {
+		t.Errorf("double board should fail (false)")
+	}
+}
+
+func TestDobuleBoardRightSuccess(t *testing.T) {
+	brd, _ := board.NewGameBoard(2, 2)
+	status := brd.DoubleBoard(board.ToRight)
+	row, column := brd.GetSize()
+
+	if !(status && row == 2 && column == 4) {
+		t.Errorf("expected: status true, row 2, column 4 -"+
+			" actual: status %t, row %d, column %d", status, row, column)
+	}
+}
+
+func TestDobuleBoardRightFail(t *testing.T) {
+	brd := board.GameBoard{}
+	status := brd.DoubleBoard(board.ToRight)
+
+	if status {
+		t.Errorf("expected: false")
+	}
+}
+
+func TestDobuleBoardButtomSuccess(t *testing.T) {
+	brd, _ := board.NewGameBoard(2, 2)
+	status := brd.DoubleBoard(board.ToButtom)
+	row, column := brd.GetSize()
+
+	if !(status && row == 4 && column == 2) {
+		t.Errorf("expected: status true, row 4, column 2 -"+
+			" actual: status %t, row %d, column %d", status, row, column)
+	}
+}
+
+func TestDobuleBoardButtomFail(t *testing.T) {
+	brd := board.GameBoard{}
+	status := brd.DoubleBoard(board.ToButtom)
+
+	if status {
+		t.Errorf("expected: false")
+	}
+}
+
+func TestAddCalculateOccupiedAreaSuccess(t *testing.T) {
+	sp, _ := rectangle.NewRectangle(3, 3)
+	brd, _ := board.NewGameBoard(10, 10)
+	pos, _ := position.NewPosition(0, 0)
+
+	brd.AddShape(sp, pos)
+	area := brd.CalculateOccupiedArea()
+	if area != 9 {
+		t.Errorf("expected: 9 - actual: %d", area)
+	}
+}
+
+func TestAddCalculateOccupiedAreaFail(t *testing.T) {
+	brd := board.GameBoard{}
+	area := brd.CalculateOccupiedArea()
+	if area != -1 {
+		t.Errorf("expected: -1 - actual: %d", area)
+	}
+}
+
+func TestGetSizeSuccess(t *testing.T) {
+	brd, _ := board.NewGameBoard(10, 10)
+	row, column := brd.GetSize()
+	if !(row == 10 && column == 10) {
+		t.Errorf("expected: (10, 10) - actual: (%d, %d)", row, column)
+	}
+}
+
+func TestGetSizeFail(t *testing.T) {
+	brd := board.GameBoard{}
+	row, column := brd.GetSize()
+	if !(row == -1 && column == -1) {
+		t.Errorf("expected: (-1, -1) - actual: (%d, %d)", row, column)
+	}
+}
